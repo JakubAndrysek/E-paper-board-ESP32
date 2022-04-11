3bd21d1b6f5975bf1ea891b7e14b0e59167d2f53
Kuba Andrýsek <33494544+JakubAndrysek@users.noreply.github.com>
2022-04-11 21:23:16 +0200

    Create credentials.h

---
+++ sw/e-paper-board/src/credentials.h
@@ -0,0 +1,22 @@
+/**
+ * @file credentials.h
+ * @author Kuba Andrýsek (email@kubaandrysek.cz)
+ * @brief Přihlašovací údaje
+ * @date 2022-04-10
+ * 
+ * @copyright Copyright (c) 2022 Kuba Andrýsek
+ * 
+ */
+
+
+/**
+ * @brief Název WiFi sítě
+ * 
+ */
+const char *ssid = "ssid";
+
+/**
+ * @brief Heslo WiFi sítě
+ * 
+ */
+const char *password = "password";
